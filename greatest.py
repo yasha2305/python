83c2abb8fee3f18ea2d1fc2ef768ce7c4940f141
yasha2305 <yasha.sahucse24@ssipmt.com>
2025-12-11 00:17:11 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,16 +1,11 @@
+#This program will tell the greatest among three numbers
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
-if a > b:
+if a > b and a > c:
     print("Largest number =", a)
+elif b > c: 
+    print("Largest number =",b)
 else:
-    print("Largest number =", b)
-if a > c:
-    print("Largest number =",a)
-else:
-    print("Largest number =",c) 
-    if b>c:
-        print("Largest number =",b)
-    else:
-        print("Largest number =",c)
+    print("Largest number =",c)
