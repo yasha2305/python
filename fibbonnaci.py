6aeee9a6c17e700719657e4e9d5503ee888c4623
yasha2305 <yasha.sahucse24@ssipmt.com>
2025-12-14 21:20:16 +0530

    fibbonnaci series

---
+++ fibbonnaci.py
@@ -0,0 +1,10 @@
+#this program will generate fibbonnaci series
+n = int(input("Enter number of terms: "))
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
