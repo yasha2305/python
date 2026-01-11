3f229c4cbe7b63ccacc7b34438fbb928c5910881
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-01-11 20:56:00 +0530

    single digit or not

---
+++ checksingledig.py
@@ -0,0 +1,7 @@
+#this code will check whether the given input is a single digit number or not
+n = int(input("Enter number: "))
+
+if -9 <= n <= 9:
+    print("Single digit")
+else:
+    print("Not single digit")
