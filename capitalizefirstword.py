2c2ec80f8d0286a2eb286d363534d604c32256b2
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-01-12 17:28:48 +0530

    capitalize first letter of the sentence

---
+++ capitalizefirstword.py
@@ -0,0 +1,9 @@
+#this code will change all the first letter of a  sentence in capital letter
+s = input("Enter sentence: ")
+print(s.title())
+"""
+Enter sentence:
+hey there! my name is yasha.
+output:
+Hey There! My Name Is Yasha.hey there! my name is yasha.
+"""
