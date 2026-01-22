a43ae1835484b1ccfb06aaa65d6ce843fdf14f46
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-01-22 16:25:49 +0530

    check strings are equal or not

---
+++ cequalstring.py
@@ -0,0 +1,8 @@
+#this code will tell you that the entered two strings are equal or not
+a = input("Enter first string: ")
+b = input("Enter second string: ")
+
+if a.lower() == b.lower():
+    print("Equal")
+else:
+    print("Not equal")
