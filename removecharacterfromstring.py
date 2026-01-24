9c42e062d32bbcfc7ebedd56bab2c8426c789637
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-01-24 19:06:10 +0530

    Remove Character from a String

---
+++ removecharacterfromstring.py
@@ -0,0 +1,11 @@
+#this code will remove any desired character from a string
+s = input("Enter string: ")
+ch = input("Enter character to remove: ")
+
+print(s.replace(ch, ""))
+"""
+Enter string: yasha1506
+Enter character to remove: yasha
+output:
+1506
+"""
