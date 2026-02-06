20b9da968d937a9320317ac0294c56096e0a0bc8
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-02-06 13:56:30 +0530

    find power of a number

---
+++ finpower.py
@@ -0,0 +1,18 @@
+a = int(input("Enter base: "))
+b = int(input("Enter power: "))
+
+print("Result =", a ** b)
+"""
+example 1:
+Enter base: 7
+Enter power: 0
+output:
+Result = 1
+"""
+"""
+example 2:
+Enter base: 90
+Enter power: 3
+output:
+Result = 729000
+"""
