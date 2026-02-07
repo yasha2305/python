3aa796bd248dde39579bc9557afff9048d463f34
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-02-07 20:51:41 +0530

    print character with index

---
+++ printcharindex.py
@@ -0,0 +1,24 @@
+s = input("Enter string: ")
+
+for i in range(len(s)):
+    print(i, ":", s[i])
+"""
+Enter string: You Are My Friend
+0 : Y
+1 : o
+2 : u
+3 :
+4 : A
+5 : r
+6 : e
+7 :
+8 : M
+9 : y
+10 :
+11 : F
+12 : r
+13 : i
+14 : e
+15 : n
+16 : d
+"""
