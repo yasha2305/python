0986cb17c606744076ae9682dadd14c1b8c2d7ed
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-03-11 21:10:12 +0530

    Two different method addition

---
+++ twomethod.py
@@ -0,0 +1,13 @@
+# method to add two numbers
+def add_two(a, b):
+    sum = a + b
+    print("Sum of two numbers =", sum)
+
+# method to add three numbers
+def add_three(a, b, c):
+    sum = a + b + c
+    print("Sum of three numbers =", sum)
+
+# calling the methods
+add_two(10, 20)
+add_three(10, 20, 30)
