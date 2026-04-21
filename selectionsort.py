c019fa739f0617c6ae5133b6821d09eca6734435
yasha2305 <yasha.sahucse24@ssipmt.com>
2026-04-21 23:39:52 +0530

    Selection Sort

---
+++ selectionsort.py
@@ -0,0 +1,10 @@
+arr = [5, 2, 9, 1]
+
+for i in range(len(arr)):
+    min_index = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print(arr)
